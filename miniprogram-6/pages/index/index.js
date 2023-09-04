4c957dd2c5a49b7005b895434f1f960c21d8f224
Annyufeng <97930712+Annyufeng@users.noreply.github.com>
2023-09-04 16:59:49 +0800

    Add files via upload

---
+++ miniprogram-6/pages/index/index.js
@@ -0,0 +1,94 @@
+// pages/index/index.js
+var common = require('../../utils/common.js') //引用公共JS文件
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //幻灯片素材
+    swiperImg: [
+      {src: 'https://gaopursuit.oss-cn-beijing.aliyuncs.com/2022/newsimage1.jpg'},
+      {src: 'https://gaopursuit.oss-cn-beijing.aliyuncs.com/2022/newsimage2.jpg'},
+      {src: 'https://gaopursuit.oss-cn-beijing.aliyuncs.com/2022/newsimage3.jpg'}
+    ],
+    // newsList:[{id: '264698',
+    // title: '省退役军人事务厅来校交流对接工作',
+    // poster: 'https://gaopursuit.oss-cn-beijing.aliyuncs.com/2022/newsimage1.jpg',
+    // content: ' 8月19日，省退役军人事务厅二级巡视员蔡元和、办公室主任刘恒贵、就业创业处副处长钟俊武一行来校就联合共建安徽退役军人学院事宜进行交流对接。校党委常委、副校长陆林，芜湖市退役军人事务局党组成员、副局长张桂芬，学校办公室、人事处、教务处、招就处、学生处、研究生院、体育学院负责同志参加会议。',
+    // add_date: '2022-08-19'},]
+  },
+
+  /**
+   * 自定义函数--跳转新页面浏览新闻内容
+   */
+  goToDetail: function(e) {
+    //获取携带的data-id数据
+    let id = e.currentTarget.dataset.id;
+    //携带新闻id进行页面跳转
+    wx.navigateTo({
+      url: '../detail/detail?id=' + id
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    //获取新闻列表
+    let list = common.getNewsList()
+    //更新列表数据
+    this.setData({
+      newsList: list
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
